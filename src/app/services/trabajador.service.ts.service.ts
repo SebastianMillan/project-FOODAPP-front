79e80d01d9a4d9e126e96336bf233e566f0637d4
Pedro Franch Echaniz <pedrofe.av@gmail.com>
2023-11-23 10:03:33 +0100

    tabla

--- src/app/services/trabajador.service.ts.service.ts
+++ src/app/services/trabajador.service.ts.service.ts
@@ -14,7 +14,7 @@ export class TrabajadorServiceTsService {
     return this.http.get<TrabajadorResponse[]>('http://localhost:8080/trabajador/',
       {
         headers: {
-          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYzE5ZTAwMS04YmZiLTE3ZTItODE4Yi1mYjI5MzE4ODAwMGIiLCJpYXQiOjE3MDA3MjU4ODIsImV4cCI6MTcwMDgxMjI4Mn0.or2eWumC22DNhIrx66xXUuqvVufhEjBGmpOLF_1-e7_jiPKF9rzEXlfqI6py47CzDDHFk7NgKZxRmwpSQ169fA'
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYzE5ZTAwMS04YmZiLTFmMTgtODE4Yi1mYjUxYWE1NTAwMGIiLCJpYXQiOjE3MDA3Mjg1MjEsImV4cCI6MTcwMDgxNDkyMX0.g29Sur0qmvLtknOkfFvANV0O1GKfEdTBSmjoSghMvlrLATXcqkZrVTqa2zQAP-QE0AEY4cWhVg3mgPcGWYhRqg'
         }
       });
   }
