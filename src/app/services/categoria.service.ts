3f42dafbffbcbb456a36775b9654123e6aa5f10a
Fernando Claro <fernandocdd02@gmail.com>
2023-11-25 22:41:18 +0100

    Arreglar el boton de guardar

--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { CategoriaRowResponse } from '../models/categoria-row.interface';
 import { Observable } from 'rxjs';
 import { environment } from '../../environment/environment';
+import { NuevaCategoriaResponse } from '../models/new-categoria.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,16 @@ export class CategoriaService {
 
   getCategorias(): Observable<CategoriaRowResponse[]>{
     return this.http.get<CategoriaRowResponse[]>(`${environment.apiBaseUrl}/admin/categoria/`, {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+    });
+  }
+
+  addCategoria(nombre: string): Observable<NuevaCategoriaResponse>{
+    return this.http.post<NuevaCategoriaResponse>(`${environment.apiBaseUrl}/admin/add/categoria/`,{
+      nombre: nombre
+    },{
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
       }
--- src/app/ui/admin-categories-page/admin-categories-page.component.ts
+++ src/app/ui/admin-categories-page/admin-categories-page.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriaRowResponse } from '../../models/categoria-row.interface';
 import { CategoriaService } from '../../services/categoria.service';
+import { NuevaCategoriaResponse } from '../../models/new-categoria.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-categories-page',
   templateUrl: './admin-categories-page.component.html',
-  styleUrl: './admin-categories-page.component.css'
+  styleUrl: './admin-categories-page.component.css',
 })
-export class AdminCategoriesPageComponent implements OnInit{
-
+export class AdminCategoriesPageComponent implements OnInit {
   categorias: CategoriaRowResponse[] = [];
+  mostrarFormulario: boolean = false;
+  nombreCategoria: string = '';
 
-  constructor(private categoriaService: CategoriaService){}
-  
+  constructor(
+    private categoriaService: CategoriaService,
+    private route: Router
+  ) {}
+
   ngOnInit(): void {
-    this.categoriaService.getCategorias().subscribe(resp => {
+    this.categoriaService.getCategorias().subscribe((resp) => {
       this.categorias = resp;
-    })
+    });
   }
-  
+
+  guardarCategoria() {
+    this.categoriaService
+      .addCategoria(this.nombreCategoria)
+      .subscribe((categoria: NuevaCategoriaResponse) => {
+        alert(`Categoria ${categoria.nombre} añadida correctamente`);
+        this.actualizarListaCategorias();
+        this.route.navigate([`/admin/categorias/`]);
+      });
+  }
+
+  private actualizarListaCategorias() {
+    this.categoriaService.getCategorias().subscribe((resp) => {
+      this.categorias = resp;
+    });
+  }
+
+  cancelar() {
+    this.mostrarFormulario = false;
+  }
 }
