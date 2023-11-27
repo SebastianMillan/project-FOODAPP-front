9d22eec6b89168f09141f5d8b92e3a7c917e4229
Pedro Franch Echániz <pedrofe.av@gmail.com>
2023-11-27 16:59:25 +0100

    hasta ahora no da error en el log

---
+++ src/app/ui/form-edit-trabajador/form-edit-trabajador.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit-trabajador',
+  templateUrl: './form-edit-trabajador.component.html',
+  styleUrl: './form-edit-trabajador.component.css'
+})
+export class FormEditTrabajadorComponent {
+
+}
